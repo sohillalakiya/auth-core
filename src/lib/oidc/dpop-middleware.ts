1c073ba2ded26360bf300c12a8f132d03b0665be
Sohil Lalakiya <sohillalakiya2306@gmail.com>
2026-05-08 10:02:11 +0530

    fix: restore any[] in withDPoP generic with eslint-disable to fix build

--- src/lib/oidc/dpop-middleware.ts
+++ src/lib/oidc/dpop-middleware.ts
@@ -238,7 +238,8 @@ export interface DPoPMiddlewareConfig {
  * });
  * ```
  */
-export function withDPoP<T extends unknown[]>(
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function withDPoP<T extends any[]>(
   config: DPoPMiddlewareConfig = {}
 ) {
   return (
